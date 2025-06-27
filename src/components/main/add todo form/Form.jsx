9ee5488e170c5c470c00637ec446aa66b3f5e6c9
Yehor <chaliyegor00@gmail.com>
2025-06-27 14:49:13 +0300

    fine-tune form markup and add styles for the form

--- src/components/main/add todo form/Form.jsx
+++ src/components/main/add todo form/Form.jsx
@@ -3,13 +3,15 @@ import './Form.css'
 export default function Form() {
 	function formSubmitHandle(formData) {
 		const todo = formData.get('add-todo')
-		console.log(todo);
+		console.log(todo)
 	}
 
 	return (
 		<form action={formSubmitHandle}>
-			<input name='add-todo' type="text" className="add-todo" />
-			<button type="submit">add</button>
+			<div className='info-wrapper'>
+				<input name='add-todo' type='text' className='add-todo' placeholder='add a task...'/>
+				<button type='submit'>add</button>
+			</div>
 		</form>
 	)
 }
