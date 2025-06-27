5d474c1c9506d4b52670dc925d6550c23d93f427
Yehor <chaliyegor00@gmail.com>
2025-06-27 16:01:11 +0300

    set todo value to the allTodos state and add nanoid

--- src/components/main/add todo form/Form.jsx
+++ src/components/main/add todo form/Form.jsx
@@ -1,15 +1,27 @@
 import './Form.css'
+import { nanoid } from 'nanoid'
 
-export default function Form() {
+export default function Form({ setAllTodos }) {
 	function formSubmitHandle(formData) {
-		const todo = formData.get('add-todo')
-		console.log(todo)
+		const getTodoValue = formData.get('add-todo')
+
+		if (getTodoValue.trim() !== '') {
+			setAllTodos(prevTodos => [
+				...prevTodos,
+				{ id: nanoid(), value: getTodoValue, isDone: false },
+			])
+		}
 	}
 
 	return (
 		<form action={formSubmitHandle}>
 			<div className='info-wrapper'>
-				<input name='add-todo' type='text' className='add-todo' placeholder='add a task...'/>
+				<input
+					name='add-todo'
+					type='text'
+					className='add-todo'
+					placeholder='add a task...'
+				/>
 				<button type='submit'>add</button>
 			</div>
 		</form>
