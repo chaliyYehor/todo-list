806f7222df98ca04aa74a7bead5f2db1517007df
Yehor <chaliyegor00@gmail.com>
2025-06-27 16:00:36 +0300

    add state for all todos

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,15 @@
 import Form from './add todo form/Form.jsx'
 import TodoList from './todo-list/TodoList.jsx';
 import './Main.css'
+import { useState } from 'react'
 
 export default function Main() {
+	const [allTodos, setAllTodos] = useState([])
+
 	return (
 		<main>
-			<Form />
-			<TodoList />
+			<Form setAllTodos={setAllTodos}/>
+			<TodoList allTodos={allTodos}/>
 		</main>
 	)
 }
