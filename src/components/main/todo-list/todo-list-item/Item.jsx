fadb28cb1e2d3424be13e0ef3558c9585269e493
Yehor <chaliyegor00@gmail.com>
2025-06-27 15:05:43 +0300

    add final markup

--- src/components/main/todo-list/todo-list-item/Item.jsx
+++ src/components/main/todo-list/todo-list-item/Item.jsx
@@ -1,7 +1,13 @@
 import './Item.css'
 
-export default function Item() {
+export default function Item({ value }) {
 	return (
-		<div>Item</div>
+		<li>
+			<span>{value}</span>
+			<section className='actions'>
+				<i className="fa-solid fa-trash"></i>
+				<i className="fa-solid fa-check"></i>
+			</section>
+		</li>
 	)
 }
