341a678b902d7e573c2dbf228f309298b68ca300
Yehor <chaliyegor00@gmail.com>
2025-06-27 14:52:07 +0300

    render todo-list component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import Form from './add todo form/Form.jsx'
+import TodoList from './todo-list/TodoList.jsx';
 import './Main.css'
 
 export default function Main() {
 	return (
 		<main>
 			<Form />
+			<TodoList />
 		</main>
 	)
 }
